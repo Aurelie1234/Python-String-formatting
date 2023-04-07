ebf8e4a1cc56afaaf395a869a3137a19a3129551
Aurelie1234 <115810657+Aurelie1234@users.noreply.github.com>
2023-04-07 15:34:23 +0000

    Create Python String formatting.py

---
+++ Python String formatting.py
@@ -0,0 +1,6 @@
+def print_formatted(number):
+    # your code goes here
+ 
+    l = len(bin(number)[2:])
+    for i in range(1, number+1):
+        print(str(i).rjust(l), str(oct(i)[2:]).rjust(l), str(hex(i).upper()[2:]).rjust(l), str(bin(i)[2:]).rjust(l))
